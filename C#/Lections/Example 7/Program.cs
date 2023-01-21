128a8b58533d091d2a9c79bbc0bf036c66178426
Leprikon <mostrader2014@yandex.ru>
2023-01-21 23:06:11 +0300

    Добавлен Калькулятор Random

---
+++ C#/Lections/Example 7/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = new Random().Next(1,1000);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,1000);
+Console.WriteLine(numberB);
+int result = (numberA + numberB);
+Console.WriteLine(result);
+
+//      Сложнение случайно генерируемых чисел
+//                         за это отвечает команда
+//                                            new.Random().next(x,y)
