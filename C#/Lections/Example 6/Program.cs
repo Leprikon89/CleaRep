8079fc35720ea9c463de26c2df593af5ef1e06c7
Leprikon <mostrader2014@yandex.ru>
2023-01-21 19:37:03 +0300

    Double -работа с вещественными числами

---
+++ C#/Lections/Example 6/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = 12;
+double numberB = 5;
+double result = (numberA / numberB);
+Console.WriteLine(result);
+
+// Переменная **int**(Целые числа) заменена переменной **double**
+//                     (Вещественные числа) чтобы обрабатывать
+//                               Дробные значения и значения в степени
