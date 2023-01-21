0e4824369436adaa349d2422fd16886719de3f59
Leprikon <mostrader2014@yandex.ru>
2023-01-21 13:28:42 +0300

    торая версия калькулятора с коментом

---
+++ C#/Lections/Example 5/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 3;
+int numberB = 5;
+int result = (numberA + numberB);
+Console.WriteLine(result);
+
+// Добавлен контейнер **(Result) В котором хранятся данные вычислений
+//                      затем выводящиеся на экран.
