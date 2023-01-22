9f8dcd51bcb871f9908aece49362df8d0ffe3305
Leprikon <mostrader2014@yandex.ru>
2023-01-22 17:34:18 +0300

    Сравнение чисел. Нахождение максимума

---
+++ C#/Lections/Lection1/Example 9/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 2;
+int b = 4;
+int c = 7;
+int d = 5;                       // Нахождение максимума
+int e = 8;
+int max = a;
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+Console.Write(max);
