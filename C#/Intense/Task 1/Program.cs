12678b618684a5f26b3e5aa266fe9db4a192a6f6
Leprikon <mostrader2014@yandex.ru>
2023-01-22 21:59:43 +0300

    ToInt32 и нахождение квадрата числа

---
+++ C#/Intense/Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int x =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат числа =");
+Console.Write(x*x);
+
+//                     Перевели данные от пользователя
+//                                 Из строкового в целочисленный формат
+//                                                 Командой **ToInt32**
