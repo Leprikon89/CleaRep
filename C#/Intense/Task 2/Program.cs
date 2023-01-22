01fcfe92b7d03e1dfa1476c7d9eb71dd10041aa0
Leprikon <mostrader2014@yandex.ru>
2023-01-22 22:50:24 +0300

    if добавлено ветвление

---
+++ C#/Intense/Task 2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int x =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат числа =");
+Console.Write(x*x);
+if (x>9&&x<100)
+{
+  Console.WriteLine("Вы ввели двузначное число!!!!");  
+}
+
+// Применение цикла if
+//          Ветвление
