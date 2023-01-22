b2f6d19c21ceb34c5ba392cc54b0b7b9b45fed9e
Leprikon <mostrader2014@yandex.ru>
2023-01-22 15:24:25 +0300

    абота с условиями

---
+++ C#/Lections/Examle 8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите Имя");
+String username = Console.ReadLine();
+
+if (username == "Снежа")
+{
+ Console.Write("Ура");   
+}
+else
+{
+    Console.Write("Нах");
+}
+
+//                                         Применение условий если... то...
