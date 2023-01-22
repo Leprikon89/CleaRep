f16c6ee6eb3c7367e2faa99bf161a8e1d2da722e
Leprikon <mostrader2014@yandex.ru>
2023-01-22 23:46:32 +0300

    if & else

---
+++ C#/Intense/Task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x == y*y)
+{
+    Console.WriteLine("Это квадрат");
+}
+else
+{
+    Console.WriteLine("Это Не квадрат");
+}
