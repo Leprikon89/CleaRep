775bb283bdd932044507bad6d8ed147a54b9b551
Leprikon <mostrader2014@yandex.ru>
2023-01-25 19:41:57 +0300

    Убираем число через строку

---
+++ C#/Intense/Task 6/Program.cs
@@ -0,0 +1,10 @@
+﻿int x = new Random().Next(100, 1000);
+Console.WriteLine("Было сгенерировано число " +x);
+int x1 = x/100;
+int x2 = x%100;
+int x3 = x2%10;
+Console.Write(+x1);
+Console.Write(+x3);
+
+
+// Убираем цифру через строку
