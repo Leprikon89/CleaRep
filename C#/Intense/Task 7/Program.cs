22793410ef6806b05d8384e04591339e85971f95
Leprikon <mostrader2014@yandex.ru>
2023-01-28 19:16:52 +0300

    final

---
+++ C#/Intense/Task 7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int y = Convert.ToInt32(Console.ReadLine());
+int remains = y%x;
+if (remains == 0)
+{
+ Console.WriteLine("Кратно");   
+}
+else
+{
+  Console.WriteLine(" Не Кратно");  
+}
